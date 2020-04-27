616f809b843ad60cc2f12e61c75f2d466ffc63db
Passion-Logan <wangdadamailbox@163.com>
2020-04-28 00:12:59 +0800

    添加实体封装

---
+++ src/main/java/com/cody/entity/PageBean.java
@@ -0,0 +1,61 @@
+package com.cody.entity;
+
+import com.alibaba.druid.sql.dialect.mysql.ast.statement.MySqlShowProcessListStatement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ClassName: PageBean
+ *
+ * @author WQL
+ * @Description: 分页模板
+ * @date: 2020/4/27 23:59
+ * @since JDK 1.8
+ */
+public class PageBean<T> {
+
+    private List<T> pageData = new ArrayList<>();
+    private Integer pageNo = 0;
+    private Integer pageSize = Integer.valueOf(10);
+    private Long totalCount = 0L;
+
+    public PageBean(List<T> pageData, Long totalCount) {
+        this.pageData = pageData;
+        this.totalCount = totalCount;
+    }
+
+    public PageBean() {}
+
+    public Integer getPageNo() {
+        return pageNo;
+    }
+
+    public void setPageNo(Integer pageNo) {
+        this.pageNo = pageNo;
+    }
+
+    public List<T> getPageData() {
+        return this.pageData;
+    }
+
+    public void setPageData(List<T> pageData) {
+        this.pageData = pageData;
+    }
+
+    public Integer getPageSize() {
+        return this.pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public Long getTotalCount() {
+        return this.totalCount;
+    }
+
+    public void setTotalCount(Long totalCount) {
+        this.totalCount = totalCount;
+    }
+}
---
+++ src/main/java/com/cody/entity/QuartzEntity.java
@@ -0,0 +1,65 @@
+package com.cody.entity;
+
+import lombok.Data;
+
+/**
+ * ClassName: QuartzEntity
+ *
+ * @author WQL
+ * @Description:
+ * @date: 2020/4/28 0:03
+ * @since JDK 1.8
+ */
+@Data
+public class QuartzEntity {
+
+    /**
+     * 任务名称
+     */
+    private String jonName;
+
+    /**
+     * 任务分组
+     */
+    private String jobGroup;
+
+    /**
+     * 任务描述
+     */
+    private String description;
+
+    /**
+     * 执行类
+     */
+    private String jobClassName;
+
+    /**
+     * 执行方法
+     */
+    private String jonMethodName;
+
+    /**
+     * 执行时间
+     */
+    private String cronExpression;
+
+    /**
+     * 任务名称
+     */
+    private String triggerName;
+
+    /**
+     * 任务状态
+     */
+    private String triggerState;
+
+    /**
+     * 任务名称 用于修改
+     */
+    private String oldJobName;
+
+    /**
+     * 任务分组 用于修改
+     */
+    private String oldJonGroup;
+}
